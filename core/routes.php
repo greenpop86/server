f5ef66125cbe179952bf245a1ed6353b39fa86e9
Lukas Reschke <lukas@statuscode.ch>
2016-11-17 17:35:14 +0100

    Add identity proof
    Signed-off-by: Lukas Reschke <lukas@statuscode.ch>

--- core/routes.php
+++ core/routes.php
@@ -60,6 +60,7 @@ $application->registerRoutes($this, [
 		['root' => '/cloud', 'name' => 'OCS#getCurrentUser', 'url' => '/user', 'verb' => 'GET'],
 		['root' => '', 'name' => 'OCS#getConfig', 'url' => '/config', 'verb' => 'GET'],
 		['root' => '/person', 'name' => 'OCS#personCheck', 'url' => '/check', 'verb' => 'POST'],
+		['root' => '/identityproof', 'name' => 'OCS#getIdentityProof', 'url' => '/key/{cloudId}', 'verb' => 'GET'],
 	],
 ]);
 
